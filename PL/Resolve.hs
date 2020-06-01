112599875969b4bc4c3f7b0ee6015644e7364e1c
Samuel A. Yallop <syallop@gmail.com>
2020-06-01 18:33:09 +0100

    Add Hash shortening functionality to Resolve stage

--- PL/Resolve.hs
+++ PL/Resolve.hs
@@ -12,10 +12,12 @@ Copyright   : (c) Samuel A. Yallop, 2020
 Maintainer  : syallop@gmail.com
 Stability   : experimental
 
-This module defines a compiler phase which can be used to resolve ShortHashes
-within {expr,type,pattern}s to unambiguous Hashes in the context of some CodeStore.
+This module defines a compiler phase which can be used to:
+- Resolve ShortHashes within {expr,type,pattern}s to unambiguous Hashes
+- Shorten Hashes within {expr,type,pattern}s to the shortest -currently unambiguous- ShortHash
 
 Resolution would typically take place after parsing and before type-checking.
+Shortening would typically take place before printing.
 
 -}
 module PL.Resolve
@@ -39,15 +41,35 @@ module PL.Resolve
   , resolveTypeHash
   , resolveKindHash
 
+  -- Shorten Hashes within {expr,type,pattern}s to the smallest - currently -
+  -- unambiguous ShortHash.
+  , shortenHashes
+  , shortenTypeHashes
+  , shortenPatternHashes
+
+  , shortenCaseHashes
+  , shortenCaseBranchesHashes
+  , shortenCaseBranchHashes
+
+  -- Shorten individual hashes to the smallest unambiguous hash
+  , shortenExprHash
+  , shortenTypeHash
+  , shortenKindHash
+
   -- Claim that two phases are identical with one using ShortHashes and the
   -- other resolved Hashes.
   , ExprWithResolvedHashes
   , TypeWithResolvedHashes
   , PatternWithResolvedHashes
+
+  -- Inverse of *WithResolvedHashes
+  , ExprWithShortenedHashes
+  , TypeWithShortenedHashes
+  , PatternWithShortenedHashes
   )
   where
 
-import PL.CodeStore
+import qualified PL.CodeStore as C
 import PL.Error
 import PL.Expr
 import PL.Case
@@ -65,13 +87,13 @@ import qualified Data.Set as Set
 -- | Contains information used when resolving names within expressions, types
 -- and patterns.
 data ResolveCtx = ResolveCtx
-  { _resolveCodeStore :: CodeStore -- ^ Backing CodeStore consulted to map ShortHashes to Hashes.
+  { _resolveCodeStore :: C.CodeStore -- ^ Backing CodeStore consulted to map ShortHashes to Hashes.
   }
 
 -- | Create a ResolveCtx with a CodeStore that can be consulted as the authority
 -- on whether ShortHashes are prefixes of any larger Hashes.
 mkResolveCtx
-  :: CodeStore
+  :: C.CodeStore
   -> ResolveCtx
 mkResolveCtx codeStore = ResolveCtx
   { _resolveCodeStore = codeStore
@@ -143,7 +165,7 @@ resolveExprHash
   :: ShortHash
   -> Resolve Hash
 resolveExprHash shortHash = Resolve $ \ctx -> do
-  mRes <- largerExprHashes (_resolveCodeStore ctx) shortHash
+  mRes <- C.largerExprHashes (_resolveCodeStore ctx) shortHash
   let errCtx = EContext (EMsg . mconcat $ [text "Resolving expression short-hash:"
                                           ,lineBreak
                                           ,indent1 . string . show $ shortHash
@@ -175,7 +197,7 @@ resolveTypeHash
   :: ShortHash
   -> Resolve Hash
 resolveTypeHash shortHash = Resolve $ \ctx -> do
-  mRes <- largerTypeHashes (_resolveCodeStore ctx) shortHash
+  mRes <- C.largerTypeHashes (_resolveCodeStore ctx) shortHash
   let errCtx = EContext (EMsg . mconcat $ [text "Resolving type short-hash:"
                                           ,lineBreak
                                           ,indent1 . string . show $ shortHash
@@ -207,7 +229,7 @@ resolveKindHash
   :: ShortHash
   -> Resolve Hash
 resolveKindHash shortHash = Resolve $ \ctx -> do
-  mRes <- largerKindHashes (_resolveCodeStore ctx) shortHash
+  mRes <- C.largerKindHashes (_resolveCodeStore ctx) shortHash
   let errCtx = EContext (EMsg . mconcat $ [text "Resolving kind short-hash:"
                                           ,lineBreak
                                           ,indent1 . string . show $ shortHash
@@ -229,6 +251,58 @@ resolveKindHash shortHash = Resolve $ \ctx -> do
                  [ text "More than one hash with the given prefix was found. did you mean one of these?"
                  , string . show $ hashes
                  ]
+
+-- | Given an Expr Hash, shorten it to the shortest unambiguous ShortHash.
+shortenExprHash
+  :: Hash
+  -> Resolve ShortHash
+shortenExprHash exprHash = Resolve $ \ctx -> do
+  mRes <- C.shortenExprHash (_resolveCodeStore ctx) exprHash
+  case mRes of
+    Nothing
+      -> pure . Left . EContext (EMsg . mconcat $ [ text "Shortening expr hash:"
+                                                  , lineBreak
+                                                  , indent1 . string . show $ exprHash
+                                                  ]
+                                ) $ EMsg $ text "Unknown error in underlying code store"
+
+    Just (codeStore', shortHash)
+      -> pure . Right $ (ctx{_resolveCodeStore=codeStore'}, shortHash)
+
+-- | Given a Type Hash, shorten it to the shortest unambiguous ShortHash.
+shortenTypeHash
+  :: Hash
+  -> Resolve ShortHash
+shortenTypeHash typeHash = Resolve $ \ctx -> do
+  mRes <- C.shortenTypeHash (_resolveCodeStore ctx) typeHash
+  case mRes of
+    Nothing
+      -> pure . Left . EContext (EMsg . mconcat $ [ text "Shortening type hash:"
+                                                  , lineBreak
+                                                  , indent1 . string . show $ typeHash
+                                                  ]
+                                ) $ EMsg $ text "Unknown error in underlying code store"
+
+    Just (codeStore', shortHash)
+      -> pure . Right $ (ctx{_resolveCodeStore=codeStore'}, shortHash)
+
+-- | Given a Kind Hash, shorten it to the shortest unambiguous ShortHash.
+shortenKindHash
+  :: Hash
+  -> Resolve ShortHash
+shortenKindHash kindHash = Resolve $ \ctx -> do
+  mRes <- C.shortenKindHash (_resolveCodeStore ctx) kindHash
+  case mRes of
+    Nothing
+      -> pure . Left . EContext (EMsg . mconcat $ [ text "Shortening kind hash:"
+                                                  , lineBreak
+                                                  , indent1 . string . show $ kindHash
+                                                  ]
+                                ) $ EMsg $ text "Unknown error in underlying code store"
+
+    Just (codeStore', shortHash)
+      -> pure . Right $ (ctx{_resolveCodeStore=codeStore'}, shortHash)
+
 
 -- | Two expression phases are identical but:
 -- - The first uses potentially ambiguous ShortHashes
@@ -261,6 +335,11 @@ type ExprWithResolvedHashes unresolved resolved =
   , TypeBindingFor    unresolved ~ TypeBindingFor    resolved
   )
 
+-- | Two expression phases are identical but:
+-- - The first uses unambiguous Hashes
+-- - The second uses shortened - potentially ambiguous - ShortHashes
+type ExprWithShortenedHashes long short = ExprWithResolvedHashes short long
+
 -- | Two type phases are identical but:
 -- - The first uses potentially ambiguous ShortHashes
 -- - The second uses unambiguous Hashes
@@ -269,6 +348,7 @@ type TypeWithResolvedHashes unresolved resolved =
   , TypeContentBindingFor resolved   ~ ContentName
 
   , Ord (TypeFor resolved)
+  , Ord (TypeFor unresolved)
 
     -- Otherwise both types have the same extensions
   , NamedExtension              unresolved ~ NamedExtension              resolved
@@ -286,6 +366,11 @@ type TypeWithResolvedHashes unresolved resolved =
     -- Both types use the same type bindings
   , TypeBindingFor unresolved ~ TypeBindingFor resolved
   )
+
+-- | Two type phases are identical but:
+-- - The first uses unambiguous Hashes
+-- - The second uses shortened - potentially ambiguous - ShortHashes
+type TypeWithShortenedHashes long short = TypeWithResolvedHashes short long
 
 -- | Two pattern phases are identical but:
 -- - The first uses potentially ambiguous ShortHashes
@@ -307,6 +392,11 @@ type PatternWithResolvedHashes unresolved resolved =
     -- Both patterns use the same bindings
   , BindingFor unresolved ~ BindingFor resolved
   )
+
+-- | Two pattern phases are identical but:
+-- - The first uses unambiguous Hashes
+-- - The second uses shortened - potentially ambiguous - ShortHashes
+type PatternWithShortenedHashes long short = PatternWithResolvedHashes short long
 
 -- | Attempt to resolve all ShortHashes contained within an expression (it's,
 -- patterns and it's types) into unambiguous ContentNames.
@@ -462,4 +552,160 @@ resolveTypeShortHashes = \case
     -> pure $ TypeExtensionExt ext
 
   _ -> error "Non-exhaustive pattern in stripTypeComments"
+
+-- | Attempt to shorten all Hashes contained within an expression (it's patterns
+-- and it's types) into the shortest unambiguous ShortHashes.
+shortenHashes
+  :: ( ExprWithShortenedHashes long short
+     , AbstractionFor long ~ TypeFor typePhase
+     , AbstractionFor short ~ TypeFor typePhase
+     )
+  => ExprFor long
+  -> Resolve (ExprFor short)
+shortenHashes e = case e of
+  ContentBindingExt ext c
+    -> ContentBindingExt ext <$> (shortenExprHash . contentName $ c)
+
+  LamExt ext abs bodyExpr
+    -> LamExt ext abs <$> shortenHashes bodyExpr
+
+  AppExt ext fExpr xExpr
+    -> AppExt ext <$> shortenHashes fExpr <*> shortenHashes xExpr
+
+  CaseAnalysisExt ext c
+    -> CaseAnalysisExt ext <$> shortenCaseHashes c
+
+  SumExt ext expr ix ty
+    -> SumExt ext <$> shortenHashes expr <*> pure ix <*> mapM shortenTypeHashes ty
+
+  ProductExt ext prodExprs
+    -> ProductExt ext <$> mapM shortenHashes prodExprs
+
+  UnionExt ext unionExpr tyIx ty
+    -> UnionExt ext <$> shortenHashes unionExpr
+                    <*> shortenTypeHashes tyIx
+                    <*> (fmap Set.fromList $ mapM shortenTypeHashes $ Set.toList ty)
+
+  BindingExt ext b
+    -> pure $ BindingExt ext b
+
+  BigLamExt ext takeTy expr
+    -> BigLamExt ext takeTy <$> shortenHashes expr
+
+  BigAppExt ext fExpr xTy
+    -> BigAppExt ext <$> shortenHashes fExpr <*> shortenTypeHashes xTy
+
+  ExprExtensionExt ext
+    -> pure $ ExprExtensionExt ext
+
+  _ -> error "Non-exhaustive pattern shortening hashes in expression"
+
+-- | Attempt to shorten all Hashes contained within a case expression
+-- to the shortest unambiguous ShortHashes.
+shortenCaseHashes
+  :: ( ExprWithShortenedHashes long short
+     , AbstractionFor long ~ TypeFor typePhase
+     , AbstractionFor short ~ TypeFor typePhase
+     )
+  => Case (ExprFor long) (PatternFor long)
+  -> Resolve (Case (ExprFor short) (PatternFor short))
+shortenCaseHashes (Case expr branches) = Case <$> shortenHashes expr <*> shortenCaseBranchesHashes branches
+
+-- | Attempt to shorten all Hashes contained within case branches
+-- to the shortest unambiguous ShortHashes.
+shortenCaseBranchesHashes
+  :: ( ExprWithShortenedHashes long short
+     , AbstractionFor long ~ TypeFor typePhase
+     , AbstractionFor short ~ TypeFor typePhase
+     )
+  => CaseBranches (ExprFor long) (PatternFor long)
+  -> Resolve (CaseBranches (ExprFor short) (PatternFor short))
+shortenCaseBranchesHashes = \case
+  DefaultOnly expr
+    -> DefaultOnly <$> shortenHashes expr
+
+  CaseBranches neBranches mDefault
+    -> CaseBranches <$> mapM shortenCaseBranchHashes neBranches
+                    <*> mapM shortenHashes mDefault
+
+-- | Attempt to shorten all Hashes contained within a case branch
+-- to the shortest unambiguous ShortHashes.
+shortenCaseBranchHashes
+  :: ( ExprWithShortenedHashes long short
+     , AbstractionFor long ~ TypeFor typePhase
+     , AbstractionFor short ~ TypeFor typePhase
+     )
+  => CaseBranch (ExprFor long) (PatternFor long)
+  -> Resolve (CaseBranch (ExprFor short) (PatternFor short))
+shortenCaseBranchHashes (CaseBranch match result) = CaseBranch <$> shortenPatternHashes match <*> shortenHashes result
+
+-- | Attempt to shorten all Hashes contained within a pattern
+-- to the shortest unambiguous ShortHashes.
+shortenPatternHashes
+  :: PatternWithShortenedHashes long short
+  => PatternFor long
+  -> Resolve (PatternFor short)
+shortenPatternHashes = \case
+  BindExt ext
+    -> pure $ BindExt ext
+
+  BindingPatternExt ext b
+    -> pure $ BindingPatternExt ext b
+
+  SumPatternExt ext sumIndex nestedPattern
+    -> SumPatternExt ext sumIndex <$> shortenPatternHashes nestedPattern
+
+  ProductPatternExt ext nestedPatterns
+    -> ProductPatternExt ext <$> mapM shortenPatternHashes nestedPatterns
+
+  UnionPatternExt ext  unionIndexTy nestedPattern
+    -> UnionPatternExt ext <$> shortenTypeHashes unionIndexTy <*> shortenPatternHashes nestedPattern
+
+  PatternExtensionExt ext
+    -> pure $ PatternExtensionExt ext
+
+  _ -> error "Non-exhaustive pattern match when shortening hashes in pattern"
+
+-- | Attempt to shorten all Hashes contained within a type
+-- to the shortest unambiguous ShortHashes.
+shortenTypeHashes
+  :: ( TypeWithShortenedHashes long short
+     )
+  => TypeFor long
+  -> Resolve (TypeFor short)
+shortenTypeHashes = \case
+  TypeContentBindingExt ext c
+    -> TypeContentBindingExt ext <$> (shortenTypeHash . contentName $ c)
+
+  NamedExt ext tyName
+    -> pure $ NamedExt ext tyName
+
+  ArrowExt ext from to
+    -> ArrowExt ext <$> shortenTypeHashes from <*> shortenTypeHashes to
+
+  SumTExt ext types
+    -> SumTExt ext <$> mapM shortenTypeHashes types
+
+  ProductTExt ext types
+    -> ProductTExt ext <$> mapM shortenTypeHashes types
+
+  UnionTExt ext types
+    -> UnionTExt ext <$> (fmap Set.fromList . mapM shortenTypeHashes . Set.toList $ types)
+
+  BigArrowExt ext from to
+    -> BigArrowExt ext from <$> shortenTypeHashes to
+
+  TypeLamExt ext kind typ
+    -> TypeLamExt ext kind <$> shortenTypeHashes typ
+
+  TypeAppExt ext x y
+    -> TypeAppExt ext <$> shortenTypeHashes x <*> shortenTypeHashes y
+
+  TypeBindingExt ext b
+    -> pure $ TypeBindingExt ext b
+
+  TypeExtensionExt ext
+    -> pure $ TypeExtensionExt ext
+
+  _ -> error "Non-exhaustive pattern shortening hashes in type"
 
